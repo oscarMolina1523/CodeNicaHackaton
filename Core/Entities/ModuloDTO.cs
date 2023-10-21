2af76aee0ca1907d28d8c973b041edb26388c8a0
oscarMolina1523 <libreriaestudiante15@gmail.com>
2023-10-21 08:19:14 -0600

    ultimos cambios

---
+++ Core/Entities/ModuloDTO.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeNiceAplication.Core.Entities
+{
+    public class ModuloDTO
+    {
+        [BsonElement("nombreModulo")]
+        public string NombreModulo { get; set; }
+
+        [BsonElement("descripcionModulo")]
+        public string DescripcionModulo { get; set; }
+
+        [BsonElement("clases")]
+        public Clase ClasesModulo { get; set; }
+    }
+}
